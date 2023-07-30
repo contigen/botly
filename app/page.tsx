91931a21582f65181883e8047cc535fcb3eede55
contigen <69436315+contigen@users.noreply.github.com>
2023-07-30 19:45:42 +0100

    Refactor home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,18 @@
 import { Button } from '@nextui-org/button'
-import { Volume } from './(ui)/icons'
-import { ArrowRight2, Home as HomeIcon } from 'iconsax-react'
+import { Header } from './(ui)/header'
 
 export default function Home() {
     return (
         <main className=''>
+            <Header />
             <h1>
                 Unlock Pro <br />
                 Using Botly &mdash; the new
-                <span className='text-blue-600'> AI</span> dahsboard tool
+                <span className='text-blue-600'> AI</span> dashboard tool
             </h1>
             <Button>View earnings</Button>
-            <Volume />
             <div className='bg-gradient-to-tr from-[#DAC2E5] via-[#FBDDB7] to-[#D6DCD1] p-10 rounded-2xl'>
                 <h2>Available Balance</h2>
-                <span className='text-red-500'>hello 〉</span>
-                <ArrowRight2 size='32' color='#d9e3f0' />
-                <HomeIcon size='32' color='#d9e3f0' variant='TwoTone' />
             </div>
         </main>
     )
