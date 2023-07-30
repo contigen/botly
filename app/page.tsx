924e3160a18e24245ea9c3fdb3c81a0f8a33d936
contigen <69436315+contigen@users.noreply.github.com>
2023-07-30 22:03:46 +0100

    Add icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import { Button } from '@nextui-org/button'
 import { Header } from './(ui)/header'
+import { ArrowRight2 } from 'iconsax-react'
+
+const ArrowRightIcon = <ArrowRight2 size='16' />
 
 export default function Home() {
     return (
@@ -10,7 +13,7 @@ export default function Home() {
                 Using Botly &mdash; the new
                 <span className='text-blue-600'> AI</span> dashboard tool
             </h1>
-            <Button>View earnings</Button>
+            <Button>View earning {ArrowRightIcon}</Button>
             <div className='bg-gradient-to-tr from-[#DAC2E5] via-[#FBDDB7] to-[#D6DCD1] p-10 rounded-2xl'>
                 <h2>Available Balance</h2>
             </div>
