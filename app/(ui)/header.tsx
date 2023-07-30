70be69ed9fa96d600767dd8dbeae0fb8c89da09d
contigen <69436315+contigen@users.noreply.github.com>
2023-07-30 22:07:04 +0100

    Update layout
    - Add icons to header component
    - Modify  layout using flex

--- app/(ui)/header.tsx
+++ app/(ui)/header.tsx
@@ -1,14 +1,33 @@
 import FlexBox from './flex-box'
-import { ArrowLeftIcon, ArrowRightIcon } from './icons'
+import { icons } from './icons'
+import { Avatar } from '@nextui-org/avatar'
+
+const { AddIcon, ArrowLeftIcon, ArrowRightIcon, SearchIcon, NotificationIcon } =
+    icons
 
 export function Header() {
     return (
-        <FlexBox>
-            <h3>Insights</h3>
-            <FlexBox alignItems='center'>
-                {ArrowLeftIcon}
-                {ArrowRightIcon}
+        <FlexBox className='' justifyContent='between'>
+            <FlexBox className=''>
+                <h3>Insights</h3>
+                <FlexBox
+                    alignItems='center'
+                    justifyContent='around'
+                    className=''
+                >
+                    {ArrowLeftIcon}
+                    {ArrowRightIcon}
+                </FlexBox>
             </FlexBox>
+            <IconsList />
         </FlexBox>
     )
 }
+
+const IconsList = () => (
+    <FlexBox className='basis-1/4' justifyContent='evenly'>
+        {NotificationIcon}
+        {SearchIcon} {AddIcon}
+        <Avatar size='sm' isBordered isFocusable />
+    </FlexBox>
+)
