571e7d3120a8bf295e4058dd1ecfcd8ac7b1d342
contigen <69436315+contigen@users.noreply.github.com>
2023-08-03 20:14:49 +0100

    refactor code into Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
-import { Button } from '@nextui-org/button'
 import { Header } from './(ui)/header'
-import { ArrowRight2 } from 'iconsax-react'
-
-const ArrowRightIcon = <ArrowRight2 size='16' />
+import { UserCard } from './(ui)/card'
+import { Spacer } from '@nextui-org/spacer'
 
 export default function Home() {
-    return (
-        <main className=''>
-            <Header />
-            <h1>
-                Unlock Pro <br />
-                Using Botly &mdash; the new
-                <span className='text-blue-600'> AI</span> dashboard tool
-            </h1>
-            <Button endContent={ArrowRightIcon}>View earning </Button>
-            <div className='bg-gradient-to-tr from-[#DAC2E5] via-[#FBDDB7] to-[#D6DCD1] p-10 rounded-2xl'>
-                <h2>Available Balance</h2>
-            </div>
-        </main>
-    )
+  return (
+    <main className=''>
+      <Header />
+      <Spacer y={20} />
+      <UserCard />
+    </main>
+  )
 }
