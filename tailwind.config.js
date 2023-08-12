8a247df478eea7ac3edcffc580173c55dae46c66
contigen <69436315+contigen@users.noreply.github.com>
2023-08-12 03:43:46 +0100

    Reformat whitespace
    Current Prettier config reformats file's whitespace

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@
 const { nextui } = require('@nextui-org/react')
 
 module.exports = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
-        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {},
-    darkMode: 'class',
-    plugins: [nextui()],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {},
+  darkMode: 'class',
+  plugins: [nextui()],
 }
