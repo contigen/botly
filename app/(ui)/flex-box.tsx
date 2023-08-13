07b7476ecfe74318f5294a4726b4054fd30cf042
contigen <69436315+contigen@users.noreply.github.com>
2023-08-13 17:55:03 +0100

    Reformat whitespace

--- app/(ui)/flex-box.tsx
+++ app/(ui)/flex-box.tsx
@@ -2,54 +2,54 @@ import { tv } from 'tailwind-variants'
 import { ComponentProps as FlexBoxProps } from './types/component.type'
 
 const flexVariants = tv({
-    base: 'flex',
-    variants: {
-        direction: {
-            column: 'flex-col',
-        },
-        justifyContent: {
-            around: 'justify-around',
-            between: 'justify-between',
-            center: 'justify-center',
-            end: 'justify-end',
-            evenly: 'justify-evenly',
-        },
-        alignItems: {
-            center: 'items-center',
-            end: 'items-end',
-            baseline: 'items-baseline',
-        },
+  base: 'flex flex-wrap w-auto',
+  variants: {
+    direction: {
+      column: 'flex-col',
     },
+    justifyContent: {
+      around: 'justify-around',
+      between: 'justify-between',
+      center: 'justify-center',
+      end: 'justify-end',
+      evenly: 'justify-evenly',
+    },
+    alignItems: {
+      center: 'items-center',
+      end: 'items-end',
+      baseline: 'items-baseline',
+    },
+  },
 })
 
 export default function FlexBox({
-    children,
-    direction,
-    justifyContent,
-    alignItems,
-    className,
+  children,
+  direction,
+  justifyContent,
+  alignItems,
+  className,
 }: FlexBoxProps & {
-    direction?: 'column' | undefined
-    justifyContent?:
-        | 'center'
-        | 'around'
-        | 'between'
-        | 'end'
-        | 'evenly'
-        | undefined
-    alignItems?: 'center' | 'end' | 'baseline' | undefined
-    className?: string
+  direction?: 'column' | undefined
+  justifyContent?:
+    | 'center'
+    | 'around'
+    | 'between'
+    | 'end'
+    | 'evenly'
+    | undefined
+  alignItems?: 'center' | 'end' | 'baseline' | undefined
+  className?: string
 }) {
-    return (
-        <div
-            className={flexVariants({
-                direction,
-                justifyContent,
-                alignItems,
-                className,
-            })}
-        >
-            {children}
-        </div>
-    )
+  return (
+    <div
+      className={flexVariants({
+        direction,
+        justifyContent,
+        alignItems,
+        className,
+      })}
+    >
+      {children}
+    </div>
+  )
 }
