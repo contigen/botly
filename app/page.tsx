6ddf8e99669d69cc583725ce0ace9a2f5cca46be
contigen <69436315+contigen@users.noreply.github.com>
2023-08-15 03:22:33 +0100

    Update Home  component
    Added the Cards and Table components
    
    Updated the layout to use Flex

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import { Header } from './(ui)/header'
-import { UserCard } from './(ui)/card'
 import { Spacer } from '@nextui-org/spacer'
+import { Cards } from './(ui)/cards'
+import { Navbar } from './(ui)/navbar'
+import FlexBox from './(ui)/flex-box'
+import { DataTable } from './(ui)/data-table'
 
 export default function Home() {
   return (
-    <main className=''>
-      <Header />
-      <Spacer y={20} />
-      <UserCard />
-    </main>
+    <FlexBox className='gap-2'>
+      <Navbar />
+      <section className='flex-1 pt-2'>
+        <Header />
+        <Spacer y={20} />
+        <main>
+          <Cards />
+        </main>
+        <Spacer y={5} />
+        <DataTable />
+      </section>
+    </FlexBox>
   )
 }
