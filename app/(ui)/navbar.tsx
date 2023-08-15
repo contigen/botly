e79717d50d5ce4a67addeac359c2d3801d8d3a69
contigen <69436315+contigen@users.noreply.github.com>
2023-08-15 03:19:51 +0100

    Refactor code

--- app/(ui)/navbar.tsx
+++ app/(ui)/navbar.tsx
@@ -1,6 +1,6 @@
 import { Divider } from '@nextui-org/divider'
 import { Button } from '@nextui-org/button'
-import {Spacer} from '@nextui-org/spacer'
+import { Spacer } from '@nextui-org/spacer'
 import FlexBox from './flex-box'
 import { icons } from './icons'
 import { Category2, Game, Home } from 'iconsax-react'
@@ -15,15 +15,19 @@ const ICONS = [
 
 export function Navbar() {
   return (
-    <FlexBox className='m-2 border rounded-xl bg-black/95 gap-4' direction='column' alignItems='center'>
+    <FlexBox
+      className='m-2 border rounded-xl bg-black/95 gap-4'
+      direction='column'
+      alignItems='center'
+    >
       <Image src={logo} alt='logo for botly' width={80} quality={100} />
       <Spacer y={2} />
-        {ICONS.map((icon, idx) => (
-          <Button key={idx} isIconOnly className='bg-transparent'>
-            {icon}
-            <Divider orientation='vertical' />
-          </Button>
-        ))}
+      {ICONS.map((icon, idx) => (
+        <Button key={idx} isIconOnly className='bg-transparent'>
+          {icon}
+          <Divider orientation='vertical' />
+        </Button>
+      ))}
     </FlexBox>
   )
 }
