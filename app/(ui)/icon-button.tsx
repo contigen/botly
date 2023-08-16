d967ace4d0a87413a16e01a642b65f1f06142ac8
contigen <69436315+contigen@users.noreply.github.com>
2023-08-16 20:06:49 +0100

    Add an icon only buttton component

---
+++ app/(ui)/icon-button.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@nextui-org/button";
+import { ComponentProps as IconButtonProps } from "./types/component.type";
+
+export function IconButton({children}: IconButtonProps) {
+    return <Button isIconOnly className='bg-transparent'>
+    {children}
+  </Button>
+}
