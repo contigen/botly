9e0f1c28bb3ec83153111feaa209bb717c30137d
contigen <69436315+contigen@users.noreply.github.com>
2023-08-18 19:41:15 +0100

    Update component props

--- app/(ui)/icon-button.tsx
+++ app/(ui)/icon-button.tsx
@@ -1,8 +1,13 @@
 import { Button } from "@nextui-org/button"
 import { ComponentProps as IconButtonProps } from "./types/component.type"
 
-export function IconButton({children}: IconButtonProps) {
-    return <Button isIconOnly className='bg-transparent'>
-    {children}
-  </Button>
+export function IconButton({
+  className,
+  children,
+}: IconButtonProps & { className?: string }) {
+  return (
+    <Button isIconOnly className={`bg-transparent ${className}`}>
+      {children}
+    </Button>
+  )
 }
