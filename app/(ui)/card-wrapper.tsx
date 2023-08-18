dd0690061a341b4eca62c31d232817bc803d5c0f
contigen <69436315+contigen@users.noreply.github.com>
2023-08-18 19:43:13 +0100

    Update component props

--- app/(ui)/card-wrapper.tsx
+++ app/(ui)/card-wrapper.tsx
@@ -5,7 +5,7 @@ import { ComponentProps as CardWrapperProps } from './types/component.type'
 
 export function CardWrapper({ children }: CardWrapperProps) {
   return (
-    <Card shadow='none' className='border-1 px-5 py-2 w-full'>
+    <Card shadow='none' className='border-1 px-5 py-2 w-full rounded-3xl'>
       {children}
     </Card>
   )
