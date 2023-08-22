e757dda62ad98811016336a51bd9c4f4c275f17d
contigen <69436315+contigen@users.noreply.github.com>
2023-08-22 05:11:15 +0100

    Reformat unused import

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import { NextUIProvider } from '@nextui-org/react'
-import { IconlyProvider } from 'react-iconly'
 
 export function AppUIProvider({ children }: { children: React.ReactNode }) {
-    return <NextUIProvider>{children}</NextUIProvider>
+  return <NextUIProvider>{children}</NextUIProvider>
 }
