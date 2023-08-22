843b35aa6b6dc1fdbcbd1ab49f427f4bea6ed650
contigen <69436315+contigen@users.noreply.github.com>
2023-08-22 05:13:21 +0100

    Update table component

--- app/(ui)/data-table.tsx
+++ app/(ui)/data-table.tsx
@@ -1,5 +1,6 @@
-'use client'
+"use client"
 
+import { Card, CardHeader } from "@nextui-org/card"
 import {
   Table,
   TableHeader,
@@ -7,43 +8,69 @@ import {
   TableColumn,
   TableRow,
   TableCell,
-} from '@nextui-org/table'
+} from "@nextui-org/table"
+import { Chip as NChip } from "@nextui-org/chip"
+import FlexBox from "./flex-box"
+import { ComponentProps as ChipProps } from "./types/component.type"
+
+function Chip({ active, children }: ChipProps & { active?: boolean }) {
+  return (
+    <NChip
+      classNames={{
+        base: active ? `bg-black rounded-lg` : `bg-transparent`,
+        content: active ? `text-white` : ``,
+      }}
+    >
+      {children}
+    </NChip>
+  )
+}
 
 export function DataTable() {
   return (
-    <Table aria-label='Chatbot earnings table'>
-      <TableHeader>
-        <TableColumn>Chatbot</TableColumn>
-        <TableColumn>Current pledge</TableColumn>
-        <TableColumn>Lifetime support</TableColumn>
-        <TableColumn>Available storage</TableColumn>
-      </TableHeader>
-      <TableBody>
-        <TableRow key='1'>
-          <TableCell>Tony Reichert</TableCell>
-          <TableCell>CEO</TableCell>
-          <TableCell>Active</TableCell>
-          <TableCell>Active</TableCell>
-        </TableRow>
-        <TableRow key='2'>
-          <TableCell>Zoey Lang</TableCell>
-          <TableCell>Technical Lead</TableCell>
-          <TableCell>Paused</TableCell>
-          <TableCell>Paused</TableCell>
-        </TableRow>
-        <TableRow key='3'>
-          <TableCell>Jane Fisher</TableCell>
-          <TableCell>Senior Developer</TableCell>
-          <TableCell>Active</TableCell>
-          <TableCell>Active</TableCell>
-        </TableRow>
-        <TableRow key='4'>
-          <TableCell>William Howard</TableCell>
-          <TableCell>Community Manager</TableCell>
-          <TableCell>Vacation</TableCell>
-          <TableCell>Vacation</TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <Card shadow='none' className='p-4 border rounded-3xl'>
+      <CardHeader>
+        <FlexBox
+          alignItems='center'
+          justifyContent='between'
+          className='w-full'
+        >
+          <h4>Earnings</h4>
+          <FlexBox
+            justifyContent='end'
+            alignItems='baseline'
+            className='gap-3 [&>*]:text-gray-400'
+          >
+            <Chip active>1D</Chip>
+            <Chip>1W</Chip>
+            <Chip>1M</Chip>
+            <Chip>1Y</Chip>
+            <span className='text-sm'>All time</span>
+          </FlexBox>
+        </FlexBox>
+      </CardHeader>
+      <Table removeWrapper aria-label='Chatbot earnings table'>
+        <TableHeader>
+          <TableColumn>Chatbot</TableColumn>
+          <TableColumn>Current pledge</TableColumn>
+          <TableColumn>Lifetime support</TableColumn>
+          <TableColumn>Available storage</TableColumn>
+        </TableHeader>
+        <TableBody>
+          <TableRow key='1'>
+            <TableCell>Tony Reichert</TableCell>
+            <TableCell>$80.00</TableCell>
+            <TableCell>Active</TableCell>
+            <TableCell>128/512 Gb</TableCell>
+          </TableRow>
+          <TableRow key='2'>
+            <TableCell>Zoey Lang</TableCell>
+            <TableCell>$160.00</TableCell>
+            <TableCell>Paused</TableCell>
+            <TableCell>256/512 Gb</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </Card>
   )
 }
